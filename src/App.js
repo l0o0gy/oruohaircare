7490daff0e5b44d778b8552f4fba29cbefa893fa
l0o0gy <minamahone9620@gmail.com>
2024-09-20 23:59:53 +0300

    remove deploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename="/oruohaircare">
+      <BrowserRouter>
         <AppBar />
         <DataProvider>
           <Routes>
